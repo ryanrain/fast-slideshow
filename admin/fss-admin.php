39c979acfdfd31532e4a6c2d427e4065544ec4a4
Ryan Wadsworth <ryanrain@gmail.com>
2016-01-03 23:44:57 -0600

    1. move call to lory into an inline script, 2. allow user to define homepage and internal page slide duration, 3. only create the shortcode markup on single pages (not listings)

--- admin/fss-admin.php
+++ admin/fss-admin.php
@@ -47,6 +47,22 @@ function fss_settings_init(  ) {
 		'fss_pluginPage_section' 
 	);
 
+	add_settings_field( 
+		'fss_homepage_slide_duration', 
+		__( 'Slide duration on the homepage', 'fss' ), 
+		'fss_homepage_slide_duration', 
+		'pluginPage', 
+		'fss_pluginPage_section' 
+	);
+
+	add_settings_field( 
+		'fss_single_slide_duration', 
+		__( 'Slide duration on internal pages', 'fss' ), 
+		'fss_single_slide_duration', 
+		'pluginPage', 
+		'fss_pluginPage_section' 
+	);
+
 
 }
 
@@ -56,8 +72,8 @@ function fss_settings_section_callback(  ) {
 
 }
 
+// fallback just in case no settings are present
 $fss_options = ( get_option( 'fss_settings') ) ? get_option( 'fss_settings') : array();
-
 
 function fss_js_checkbox(  ) { 
 	global $fss_options;
@@ -73,6 +89,62 @@ function fss_css_checkbox(  ) {
 	<?php
 }
 
+function fss_homepage_slide_duration(  ) { 
+	global $fss_options;
+	?>
+	<select name='fss_settings[fss_homepage_slide_duration]'>
+		<option value='1000' <?php selected( $fss_options['fss_homepage_slide_duration'], 1000 ); ?>>1 seconds</option>
+		<option value='2000' <?php selected( $fss_options['fss_homepage_slide_duration'], 2000 ); ?>>2 seconds</option>
+		<option value='3000' <?php selected( $fss_options['fss_homepage_slide_duration'], 3000 ); ?>>3 seconds</option>
+		<option value='4000' <?php selected( $fss_options['fss_homepage_slide_duration'], 4000 ); ?>>4 seconds</option>
+		<option value='5000' <?php selected( $fss_options['fss_homepage_slide_duration'], 5000 ); ?>>5 seconds</option>
+		<option value='6000' <?php selected( $fss_options['fss_homepage_slide_duration'], 6000 ); ?>>6 seconds</option>
+		<option value='7000' <?php selected( $fss_options['fss_homepage_slide_duration'], 7000 ); ?>>7 seconds</option>
+		<option value='8000' <?php selected( $fss_options['fss_homepage_slide_duration'], 8000 ); ?>>8 seconds</option>
+		<option value='9000' <?php selected( $fss_options['fss_homepage_slide_duration'], 9000 ); ?>>9 seconds</option>
+		<option value='10000' <?php selected( $fss_options['fss_homepage_slide_duration'], 10000 ); ?>>10 seconds</option>
+		<option value='11000' <?php selected( $fss_options['fss_homepage_slide_duration'], 11000 ); ?>>11 seconds</option>
+		<option value='12000' <?php selected( $fss_options['fss_homepage_slide_duration'], 12000 ); ?>>12 seconds</option>
+		<option value='13000' <?php selected( $fss_options['fss_homepage_slide_duration'], 13000 ); ?>>13 seconds</option>
+		<option value='14000' <?php selected( $fss_options['fss_homepage_slide_duration'], 14000 ); ?>>14 seconds</option>
+		<option value='15000' <?php selected( $fss_options['fss_homepage_slide_duration'], 15000 ); ?>>15 seconds</option>
+		<option value='16000' <?php selected( $fss_options['fss_homepage_slide_duration'], 16000 ); ?>>16 seconds</option>
+		<option value='17000' <?php selected( $fss_options['fss_homepage_slide_duration'], 17000 ); ?>>17 seconds</option>
+		<option value='18000' <?php selected( $fss_options['fss_homepage_slide_duration'], 18000 ); ?>>18 seconds</option>
+		<option value='19000' <?php selected( $fss_options['fss_homepage_slide_duration'], 19000 ); ?>>19 seconds</option>
+		<option value='20000' <?php selected( $fss_options['fss_homepage_slide_duration'], 20000 ); ?>>20 seconds</option>
+	</select>
+<?php
+}
+
+
+function fss_single_slide_duration(  ) { 
+	global $fss_options;
+	?>
+	<select name='fss_settings[fss_single_slide_duration]'>
+		<option value='1000' <?php selected( $fss_options['fss_single_slide_duration'], 1000 ); ?>>1 seconds</option>
+		<option value='2000' <?php selected( $fss_options['fss_single_slide_duration'], 2000 ); ?>>2 seconds</option>
+		<option value='3000' <?php selected( $fss_options['fss_single_slide_duration'], 3000 ); ?>>3 seconds</option>
+		<option value='4000' <?php selected( $fss_options['fss_single_slide_duration'], 4000 ); ?>>4 seconds</option>
+		<option value='5000' <?php selected( $fss_options['fss_single_slide_duration'], 5000 ); ?>>5 seconds</option>
+		<option value='6000' <?php selected( $fss_options['fss_single_slide_duration'], 6000 ); ?>>6 seconds</option>
+		<option value='7000' <?php selected( $fss_options['fss_single_slide_duration'], 7000 ); ?>>7 seconds</option>
+		<option value='8000' <?php selected( $fss_options['fss_single_slide_duration'], 8000 ); ?>>8 seconds</option>
+		<option value='9000' <?php selected( $fss_options['fss_single_slide_duration'], 9000 ); ?>>9 seconds</option>
+		<option value='10000' <?php selected( $fss_options['fss_single_slide_duration'], 10000 ); ?>>10 seconds</option>
+		<option value='11000' <?php selected( $fss_options['fss_single_slide_duration'], 11000 ); ?>>11 seconds</option>
+		<option value='12000' <?php selected( $fss_options['fss_single_slide_duration'], 12000 ); ?>>12 seconds</option>
+		<option value='13000' <?php selected( $fss_options['fss_single_slide_duration'], 13000 ); ?>>13 seconds</option>
+		<option value='14000' <?php selected( $fss_options['fss_single_slide_duration'], 14000 ); ?>>14 seconds</option>
+		<option value='15000' <?php selected( $fss_options['fss_single_slide_duration'], 15000 ); ?>>15 seconds</option>
+		<option value='16000' <?php selected( $fss_options['fss_single_slide_duration'], 16000 ); ?>>16 seconds</option>
+		<option value='17000' <?php selected( $fss_options['fss_single_slide_duration'], 17000 ); ?>>17 seconds</option>
+		<option value='18000' <?php selected( $fss_options['fss_single_slide_duration'], 18000 ); ?>>18 seconds</option>
+		<option value='19000' <?php selected( $fss_options['fss_single_slide_duration'], 19000 ); ?>>19 seconds</option>
+		<option value='20000' <?php selected( $fss_options['fss_single_slide_duration'], 20000 ); ?>>20 seconds</option>
+	</select>
+<?php
+}
 
 function fss_options_page(  ) { 
 	include FSS_PLUGIN_DIR . 'admin/admin-template.php';
--- public/fss-public.php
+++ public/fss-public.php
@@ -20,32 +20,87 @@ function fss_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'fss_scripts');
 
+function fss_inline_script () {
+	global $fss_options;
+
+	if(is_home() || is_front_page()) {
+		$slide_duration .= $fss_options['fss_homepage_slide_duration'];
+	} else {
+		$slide_duration .= $fss_options['fss_single_slide_duration'];
+	}
+
+	$script = 
+	"<script>
+	function ready(fn) {
+	  if (document.readyState !== 'loading'){
+	    fn();
+	  } else {
+	    document.addEventListener('DOMContentLoaded', fn);
+	  }
+	} 
+	ready(function(){
+		var sliderMarkup = document.querySelector('.js_slider'),
+			sliderAuto = true,
+			sliderLory = lory(sliderMarkup, {
+			    infinite: 1, 
+			    // enableMouseEvents: true
+			});
+		
+		sliderLory.setup(); // firefox bug?
+
+		// autoscroll
+		window.setInterval(function(){
+			if (sliderAuto) {
+				sliderLory.next();
+			}
+		}, $slide_duration);
+
+		// pause autoscroll if user has manually interacted with slider
+		var sliderPrev = document.querySelector('.js_prev');
+		var sliderNext = document.querySelector('.js_next');
+		sliderPrev.addEventListener('click', pauseSlideshow);
+		sliderNext.addEventListener('click', pauseSlideshow);
+		sliderMarkup.addEventListener('on.lory.touchend', pauseSlideshow);
+
+		function pauseSlideshow () {
+			sliderAuto = false;
+		}
+	}); // ready
+	</script>";
+	return $script;
+}
+
 function fss_handle_shortcode() {
 	// global $fss_options;
 	global $post;
-	$args = array(
-		'post_type' => 'attachment',
-		'numberposts' => -1,
-		'post_mime_type' => 'image',
-		'post_parent' => $post->ID
-	);
-	$images = get_posts( $args );
-	$markup = '<div class="slider js_slider">
-                <div class="frame js_frame">
-                    <ul class="slides js_slides">';
-					foreach($images as $image):
-						$markup .= '<li class="js_slide">' . wp_get_attachment_image($image->ID, 'medium') . '</li>';
-					endforeach;
-	$markup .= '	</ul>
-                </div>
-				<span class="js_prev rz_slider_nav">';
-	$markup .= file_get_contents( FSS_PLUGIN_DIR . 'public/assets/prev.svg'); 
-	$markup .= '</span>
-				<span class="js_next rz_slider_nav">';
-	$markup .= file_get_contents( FSS_PLUGIN_DIR . 'public/assets/next.svg'); 
-	$markup .= '</span>';
-    $markup .= '</div>';
-    
-	return $markup;
+	if( is_a( $post, 'WP_Post' ) && is_single() && has_shortcode( $post->post_content, 'fast-slideshow') ) {
+		$args = array(
+			'post_type' => 'attachment',
+			'numberposts' => -1,
+			'post_mime_type' => 'image',
+			'post_parent' => $post->ID
+		);
+		$images = get_posts( $args );
+
+		$markup = '<div class="slider js_slider">
+	                <div class="frame js_frame">
+	                    <ul class="slides js_slides">';
+						foreach($images as $image):
+							$markup .= '<li class="js_slide">' . wp_get_attachment_image($image->ID, 'medium') . '</li>';
+						endforeach;
+		$markup .= '	</ul>
+	                </div>
+					<span class="js_prev rz_slider_nav">';
+		$markup .= file_get_contents( FSS_PLUGIN_DIR . 'public/assets/prev.svg'); 
+		$markup .= '</span>
+					<span class="js_next rz_slider_nav">';
+		$markup .= file_get_contents( FSS_PLUGIN_DIR . 'public/assets/next.svg'); 
+		$markup .= '</span>';
+	    $markup .= '</div>';
+
+	    $markup .= fss_inline_script();
+	    
+		return $markup;
+	}
 }
 add_shortcode( 'fast-slideshow', 'fss_handle_shortcode' );
